b644384617aad62b10f34bb2e98c6760ae0c1338
Tristin Harvell <tristin.harvell@go.winona.edu>
2019-01-16 20:59:22 +0000

    Added file for Tristin

---
+++ TristinHarvell.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tristin Harvell Introduction</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+			    <h1>
+				<h1 id = "message">Tristin Harvell Introduction Updated January 2018</h1>
+				<p><b>Picture</b></p>
+				<img src="images/TristinHarvell.jpg" alt="Tristin Harvell" style="width:304px">
+				<p>About:</p>
+				<ul>
+				<li><b>Approximate Age: 21</b></li>
+				<li><b>Birth Place:  Rochester, Minnesota</b></li>
+				<li>Family</li>
+				   <ul>
+				     <li>Girlfriend of 5 years</li>
+				     <li>Two younger brothers in Minnesota</li>
+				     <li> 
+				     
+				   </ul>
+				<li>Work Experience</li>
+				  <ul>
+				    <li>Hy-Vee: 2014-Present</li>
+				  </ul>
+				<li>Skills</li>
+				  <ul>
+				    <li>Java</li>
+				    <li>XML</li>
+				    <li>SQL</li>
+				    <li>HTML</li>
+				    <li>JavaScript</li>
+				    <li>CSS</li>
+				    <li>Swift</li>
+				  </ul>
+		        <li>Hobbies</li>
+		          <ul>
+				    <li>Going to music festivals and concerts.</li>
+				  </ul>
+		        <li>Family History</li>
+		          <ul>
+				    <li>Always lived in SE Minnesota.</li>
+				  </ul>
+		        <li>Other Interests/li>
+		          <ul>
+				    <li>I love the outdoors.</li>
+				    <li>I love trying new food.</li>
+				    <li>I love the traveling and going on adventures.</li>
+				  </ul>
+		        <li>Future Plans / Aspirations</li>
+		          <ul>
+				    <li>Graduate at the end of this semester.</li>
+				    <li>Get a job through Mayo Clinic (preferably as a programmer).</li>
+				    <li>Immediately continue with getting a master's degree in cyber security.</li>
+				  </ul>
+		        <li>Links</li>
+		          <ul>
+		            <li><a href="https://www.linkedin.com/in/tristin-harvell-27a632162">Tristin Harvell on LinkedIn</a></li>
+		          </ul>
+				</ul>
+	
+</body>
+</html>
