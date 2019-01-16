bcad16884290e1ce11c819140b353f8e6a0bebb8
Erika Tix <erika.tix@go.winona.edu>
2019-01-16 22:26:22 +0000

    Erika Tix Bio

---
+++ ErikaTix.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Erika Tix Introduction</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+			    <h1>
+				<h1 id = "message">Erika Tix Introduction Updated January 2019</h1>
+				<p><b>Picture</b></p>
+				<img src="images/ErikaTix.jpg" alt="Erika Tix" style="width:304px">
+				<p>About:  Required items in <b>bold</b></p>
+				<ul>
+				<li><b>Approximate Age: 26</b></li>
+				<li><b>Birth Place:  Glace Bay, Nova Scotia, Canada</b></li>
+				<li>Family</li>
+				   <ul>
+				     <li>Married for 2 years</li>
+				     <li>One son, Finley - age 6</li>
+				     <li>Two dogs, Lyra and Odin</li>
+				     
+				   </ul>
+				<li>Work Experience</li>
+				  <ul>
+				    <li>Cosmetologist since 2011</li>
+				    <li>Computer Science student - 3rd year</li>
+				  </ul>
+				<li>Skills</li>
+				  <ul>
+				    <li>Hair coloring/styling/cutting</li>
+				    <li>Creativity</li>
+				    <li>Multi-tasker extraordinaire</li>
+				    <li>Java</li>
+				    <li>SQL</li>
+				    <li>HTML</li>
+				    <li>Javascript<li>
+				  </ul>
+		        <li>Hobbies</li>
+		           <li>Painting and drawing</li>
+		           <li>Cooking and baking</li>
+		           <li>Spending time with family</li>
+		        <li>Other Interests/li>
+		        <li>Future Plans / Aspirations</li>
+		        <li>Links</li>
+		          <ul>
+		            <li><a href="https://www.facebook.com/HairByErikaTix/">Facebook</a></li>
+		          </ul>
+				</ul>
+	
+</body>
+</html>
