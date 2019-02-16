be71dd02004b11b12179a5fdd35241b868c19ec5
Kyle Aure <kaure09@winona.edu>
2019-02-16 13:00:07 -0600

    Configured embedded database using JDBC:Derby
    Updated Automated build process (mvn)
    Updated .gitignore to include more autogenerated files/structures
    Edited App.java to give an example of database usage in Java.
    Created SampleDB.java to give an example of database connectivity in Java.

---
+++ src/main/java/edu/winona/cs/db/SampleDB.java
@@ -0,0 +1,47 @@
+package edu.winona.cs.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+//TODO: Remove this class and develop a database interface to create new users. 
+public class SampleDB {
+	//connection variable to database
+	private Connection conn;
+	
+	/**
+	 * Establishes connection to our database
+	 * @throws SQLException
+	 */
+	public void connectionToDerby() throws SQLException {
+		String dbURL = "jdbc:derby:sampledb;create=true";
+		conn = DriverManager.getConnection(dbURL);
+	}
+	
+	public String sampleDBUsabe() throws SQLException {
+		Statement stmt = conn.createStatement();
+		String results = "";
+		
+		//Create Table
+		stmt.execute("Create Table USERS (id int primary key, name varchar(30))");
+		
+		//Insert two rows
+		stmt.executeUpdate("insert into USERS values (1, 'Tom')");
+		stmt.executeUpdate("insert into USERS values (2,'Peter')");
+		
+		//Query
+		ResultSet rs = stmt.executeQuery("SELECT * FROM USERS");
+		
+		//Print results
+		while (rs.next()) {
+			results += String.format("%d\t%s\n", rs.getInt("id"), rs.getString("name"));
+		}
+		
+		//Drop table
+		stmt.executeUpdate("Drop Table USERS");
+		
+		return results;
+	}
+}
