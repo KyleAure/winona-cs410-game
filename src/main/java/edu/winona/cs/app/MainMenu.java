6d945bd97a75c80e2c1bd3a9d54b283f559f6fa7
Tristin Harvell <tristin.harvell@go.winona.edu>
2019-02-18 22:14:09 +0000

    Upload MainMenu.java

---
+++ src/main/java/edu/winona/cs/app/MainMenu.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainMenu {
+ 
+    private String playerName;
+    private String playerPassword;
+    private List<Player> players = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Welcome to the Puzzle Slider Game!");
+
+    }
+}
