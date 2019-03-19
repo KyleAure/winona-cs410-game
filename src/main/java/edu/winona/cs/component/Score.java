1e8293c035f6d469dabcd332f50355d8241f6c36
Tristin Harvell <tlharvell@gmail.com>
2019-03-19 16:14:32 -0500

    modified-score

--- src/main/java/edu/winona/cs/component/Score.java
+++ src/main/java/edu/winona/cs/component/Score.java
@@ -5,23 +5,9 @@ import edu.winona.cs.log.Log;
 public class Score {
 	private static final Log LOG = new Log("Score");
 
-    private double completionTime;
     private double highScore;
 
     public Score() {
-    }
-
-    public Score(double completionTime, double highScore) {
-        this.completionTime = completionTime;
-        this.highScore = highScore;
-    }
-
-    public double getCompletionTime() {
-        return completionTime;
-    }
-
-    public void setCompletionTime(double completionTime) {
-        this.completionTime = completionTime;
     }
 
     public double getHighScore() {
@@ -34,8 +20,7 @@ public class Score {
 
     @Override
     public String toString() {
-        return "Score: " + completionTime
-                + "High Score: " + highScore;
+        return "High Score: " + highScore;
     }
 
 }
