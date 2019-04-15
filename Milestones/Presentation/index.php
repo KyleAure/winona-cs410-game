7070a0b697dcf7209e65b7dfe61039d85ff1cb74
Tristin Harvell <tlharvell@gmail.com>
2019-04-15 16:16:45 -0500

    Update Presentation Document

--- Milestones/Presentation/index.php
+++ Milestones/Presentation/index.php
@@ -3,14 +3,14 @@
 <head>
 	<title>Presentation for Team Project</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	
+
 </head>
 <body>
 	<h1>Presentation for Team Project</h1>
 	<hr>
 	<h2>Table of Contents</h2>
 	<ul>
-		<li><a href="linkHere">Link to powerpoint presentation</a></li> 
+		<li><a href="https://docs.google.com/presentation/d/1pxLRweQ7BKzvqO1v2d9kfnlTJbbIcnrOJperDr4TZpU/edit?usp=sharing">Link to powerpoint presentation</a></li> 
 	</ul>
 	<hr>
 	<p>END OF THE DOCUMENT</p>
